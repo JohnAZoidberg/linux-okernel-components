a850926d49f65f4c55417dd9b56ac454dfc194d9
Daniel Schaefer <git@danielschaefer.me>
2018-05-17 09:56:19 +0100

    Build kernel modules with nix

---
+++ default.nix
@@ -0,0 +1,42 @@
+with import <nixpkgs> {};
+let
+  buildModule =
+  { name, src, kernel ? null }:
+  stdenv.mkDerivation {
+    inherit name;
+    inherit src;
+
+    hardeningDisable = [ "pic" ];
+
+    KERNELDIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
+    INSTALL_MOD_PATH = "\${out}";
+    INSTALL_ROOT = "\${out}";
+
+    nativeBuildInputs = kernel.moduleBuildDependencies;
+
+    buildPhase = "make";
+  };
+  kernel = pkgs.linuxPackages_latest.kernel;
+in
+{
+  kvmod = buildModule {
+    inherit kernel;
+    name = "kvmod";
+    src = ./test_mappings/protected-mem/kvmod/.;
+  };
+  kwriter = buildModule {
+    inherit kernel;
+    name = "kwriter";
+    src = ./test_mappings/kwriter/.;
+  };
+  oktest-init = buildModule {
+    inherit kernel;
+    name = "oktest-init";
+    src = ./test_mappings/oktest-init/.;
+  };
+  vsys-update = buildModule {
+    inherit kernel;
+    name = "vsys-update";
+    src = ./test_mappings/vsys-update/.;
+  };
+}
